11e867280a2dc827ec5e7183a75e2c43a0b8e00f
Jack <jack3009@hotmail.co.uk>
2020-11-18 15:43:24 +0000

    created HTML interface

--- jasmine-standalone-3/spec/thermostat_spec.js
+++ jasmine-standalone-3/spec/thermostat_spec.js
@@ -1,72 +1,89 @@
-describe("Thermostat", function() {
-  var thermostat;
+"use strict";
 
+describe("Thermostat", function() {
+  
+  var thermostat;
   beforeEach(function() {
     thermostat = new Thermostat();
   });
+
   describe("#constructor", function() {
     it(".temp returns the initial temperature of 20 degrees", function() {
-      expect(thermostat.temp).toEqual(20);
+      expect(thermostat.currentTemperature()).toEqual(20);
     })
     it(".minTemp returns the initial minimum temperature of 10 degrees", function() {
-      expect(thermostat.minTemp).toEqual(10);
+      expect(thermostat.MIN_TEMP).toEqual(10);
     })
     it(".maxTemp returns the initial maximum temperature of 25 degrees", function() {
-      expect(thermostat.psmIsOn).toEqual(true);
+      expect(thermostat.psmTurnedOn()).toEqual(true);
       expect(thermostat.maxTemp).toEqual(25);
     })
-    it(".psmIsOn is true by default", function() {
-      expect(thermostat.psmIsOn).toEqual(true);
+    it(".psmTurnedOn() is true by default", function() {
+      expect(thermostat.psmTurnedOn()).toEqual(true);
     })
   })
+
   describe("#up", function() {
     it("increases the temperature by 2 degrees", function() {
-      var beforeTemp = thermostat.temp
+      var beforeTemp = thermostat.currentTemperature()
       thermostat.up(2)
-      expect(thermostat.temp).toEqual(beforeTemp + 2)
+      expect(thermostat.currentTemperature()).toEqual(beforeTemp + 2)
     })
     it("cannot increase by a 0 or negative temperature", function() {
       expect(function() {thermostat.up(-2)}).toThrowError("Up must be called with a value greater than 0");
     })
     it("cannot increase the temperature beyond the psm max temp", function() {
-      var beforeTemp = thermostat.temp
-      expect(thermostat.psmIsOn).toEqual(true)
+      var beforeTemp = thermostat.currentTemperature()
+      expect(thermostat.psmTurnedOn()).toEqual(true)
       expect(function() {thermostat.up((thermostat.maxTemp - beforeTemp) + 1)}).toThrowError("Temperature cannot go above 25")
     })
     it("cannot increase the temperature beyond the non-psm max temp", function() {
-      var beforeTemp = thermostat.temp
+      var beforeTemp = thermostat.currentTemperature()
       thermostat.psmToggle()
-      expect(thermostat.psmIsOn).toEqual(false)
+      expect(thermostat.psmTurnedOn()).toEqual(false)
       expect(function() {thermostat.up((thermostat.maxTemp - beforeTemp) + 1)}).toThrowError("Temperature cannot go above 32")
     })
+    it("increases the temperature by default amount", function() {
+      var beforeTemp = thermostat.currentTemperature()
+      thermostat.up()
+      expect(thermostat.currentTemperature()).toEqual(beforeTemp + 1)
+    })
   })
+
   describe("#down", function() {
     it("decreases the temperature by 2 degrees", function() {
-      var beforeTemp = thermostat.temp
+      var beforeTemp = thermostat.currentTemperature()
       thermostat.down(2)
-      expect(thermostat.temp).toEqual(beforeTemp - 2)
+      expect(thermostat.currentTemperature()).toEqual(beforeTemp - 2)
     })
     it("cannot decrease by a 0 or negative temperature", function() {
       expect(function() {thermostat.down(-2)}).toThrowError("Down must be called with a value greater than 0");
     })
     it("cannot drop the temperature below 10", function() {
-      var beforeTemp = thermostat.temp
-      expect(function() {thermostat.down((beforeTemp - thermostat.minTemp) + 1)}).toThrowError("Temperature cannot go below 10");
+      var beforeTemp = thermostat.currentTemperature()
+      expect(function() {thermostat.down((beforeTemp - thermostat.MIN_TEMP) + 1)}).toThrowError("Temperature cannot go below 10");
+    })
+    it("decreases the temperature by default amount", function() {
+      var beforeTemp = thermostat.currentTemperature()
+      thermostat.down()
+      expect(thermostat.currentTemperature()).toEqual(beforeTemp - 1)
     })
   })
+
   describe("#reset", function() {
     it("resets the temperature to 20", function() {
-      var beforeTemp = thermostat.temp
+      var beforeTemp = thermostat.currentTemperature()
       thermostat.up(4)
-      expect(thermostat.temp).toEqual(beforeTemp + 4)
+      expect(thermostat.currentTemperature()).toEqual(beforeTemp + 4)
       thermostat.reset()
-      expect(thermostat.temp).toEqual(20)
+      expect(thermostat.currentTemperature()).toEqual(20)
     })
   })
+
   describe("#psmToggle", function() {
     it("turns power saving mode off", function() {
       thermostat.psmToggle()
-      expect(thermostat.psmIsOn).toEqual(false);
+      expect(thermostat.psmTurnedOn()).toEqual(false);
     })
     it("changes the maximum temperature to 32 when turning off", function() {
       expect(thermostat.maxTemp).toEqual(25)
@@ -81,34 +98,35 @@ describe("Thermostat", function() {
       expect(thermostat.maxTemp).toEqual(25)
     })
   })
+
   describe("#currentEnergyUsage", function() {
     beforeEach(function() {
-      var beforeTemp = thermostat.temp
+      var beforeTemp = thermostat.currentTemperature()
     });
     it("returns low usage when temperature is 17", function() {
-      thermostat.down((thermostat.temp - 18) + 1)
-      expect(thermostat.temp).toEqual(17)
+      thermostat.down((thermostat.currentTemperature() - 18) + 1)
+      expect(thermostat.currentTemperature()).toEqual(17)
       expect(thermostat.currentEnergyUsage()).toEqual('low-usage')
     })
     it("returns medium usage when temperature is 18", function() {
-      thermostat.down((thermostat.temp - 18))
-      expect(thermostat.temp).toEqual(18)
+      thermostat.down((thermostat.currentTemperature() - 18))
+      expect(thermostat.currentTemperature()).toEqual(18)
       expect(thermostat.currentEnergyUsage()).toEqual('medium-usage')
     })
     it("returns medium usage when temperature is 24", function() {
-      thermostat.up((25 - thermostat.temp) - 1)
-      expect(thermostat.temp).toEqual(24)
+      thermostat.up((25 - thermostat.currentTemperature()) - 1)
+      expect(thermostat.currentTemperature()).toEqual(24)
       expect(thermostat.currentEnergyUsage()).toEqual('medium-usage')
     })
     it("returns medium usage when temperature is 25", function() {
-      thermostat.up((25 - thermostat.temp))
-      expect(thermostat.temp).toEqual(25)
+      thermostat.up((25 - thermostat.currentTemperature()))
+      expect(thermostat.currentTemperature()).toEqual(25)
       expect(thermostat.currentEnergyUsage()).toEqual('medium-usage')
     })
     it("returns high usage when temperature is 26", function() {
       thermostat.psmToggle()
-      thermostat.up((26 - thermostat.temp))
-      expect(thermostat.temp).toEqual(26)
+      thermostat.up((26 - thermostat.currentTemperature()))
+      expect(thermostat.currentTemperature()).toEqual(26)
       expect(thermostat.currentEnergyUsage()).toEqual('high-usage')
     });
   });
--- jasmine-standalone-3/src/thermostat.js
+++ jasmine-standalone-3/src/thermostat.js
@@ -1,25 +1,27 @@
+"use strict";
+
 function Thermostat() {
-  this.minTemp = 10;
+  this.MIN_TEMP = 10;
   this.maxTemp = 25;
   this.temp = 20;
   this.psmIsOn = true;
 };
 
-Thermostat.prototype.up = function(delta) {
+Thermostat.prototype.up = function(delta = 1) {
   if (delta <= 0) throw new Error("Up must be called with a value greater than 0");
-  if (this.temp + delta > this.maxTemp) throw new Error(`Temperature cannot go above ${this.maxTemp}`);
+  if (this.currentTemperature() + delta > this.maxTemp) throw new Error(`Temperature cannot go above ${this.maxTemp}`);
   this.temp += delta;
 };
 
-Thermostat.prototype.down = function(delta) {
+Thermostat.prototype.down = function(delta = 1 ) {
   if (delta <= 0) throw new Error("Down must be called with a value greater than 0");
-  if (this.temp - delta < this.minTemp) throw new Error("Temperature cannot go below 10");
+  if (this.currentTemperature() - delta < this.MIN_TEMP) throw new Error("Temperature cannot go below 10");
   this.temp -= delta;
 };
 
 Thermostat.prototype.psmToggle = function() {
   this.psmIsOn = !(this.psmIsOn);
-  this.maxTemp = this.psmIsOn == true ? 25 : 32;
+  this.maxTemp = this.psmTurnedOn() == true ? 25 : 32;
 }
 
 Thermostat.prototype.reset = function() {
@@ -27,5 +29,13 @@ Thermostat.prototype.reset = function() {
 }
 
 Thermostat.prototype.currentEnergyUsage = function() {
-  return this.temp < 18 ? 'low-usage' : this.temp <= 25 ? 'medium-usage' : 'high-usage';
+  return this.currentTemperature() < 18 ? 'low-usage' : this.currentTemperature() <= 25 ? 'medium-usage' : 'high-usage';
+}
+
+Thermostat.prototype.psmTurnedOn = function() {
+  return this.psmIsOn
+}
+
+Thermostat.prototype.currentTemperature = function() {
+  return this.temp
 }
