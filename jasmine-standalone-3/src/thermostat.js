7c731c7cb0b94e41c9898123699fdf6711cfb01b
Jack <jack3009@hotmail.co.uk>
2020-11-18 17:33:55 +0000

    progressed front end logic

--- jasmine-standalone-3/src/thermostat.js
+++ jasmine-standalone-3/src/thermostat.js
@@ -1,43 +1,53 @@
 "use strict";
 
-function Thermostat() {
-  this.MIN_TEMP = 10;
-  this.maxTemp = 25;
-  this.temp = 20;
-  this.psmIsOn = true;
+class Thermostat {
+  constructor() {
+    this.MIN_TEMP = 10;
+    this.maxTemp = 25;
+    this.temp = 20;
+    this.psmIsOn = true;
+  }
+  up(delta = 1) {
+    if (delta <= 0)
+      throw new Error("Up must be called with a value greater than 0");
+    if (this.currentTemperature() + delta > this.maxTemp)
+      throw new Error(`Temperature cannot go above ${this.maxTemp}`);
+    this.temp += delta;
+    return this.currentTemperature();
+  }
+  down(delta = 1) {
+    if (delta <= 0)
+      throw new Error("Down must be called with a value greater than 0");
+    if (this.currentTemperature() - delta < this.MIN_TEMP)
+      throw new Error("Temperature cannot go below 10");
+    this.temp -= delta;
+    return this.currentTemperature();
+  }
+  psmToggle() {
+    this.psmIsOn = !(this.psmIsOn);
+    this.maxTemp = this.psmTurnedOn() == true ? 25 : 32;
+  }
+  reset() {
+    this.temp = 20;
+  }
+  currentEnergyUsage() {
+    return this.currentTemperature() < 18 ? 'low-usage' : this.currentTemperature() <= 25 ? 'medium-usage' : 'high-usage';
+  }
+  psmTurnedOn() {
+    return this.psmIsOn;
+  }
+  currentTemperature() {
+    return String(this.temp);
+  }
 };
 
-Thermostat.prototype.up = function(delta = 1) {
-  if (delta <= 0) throw new Error("Up must be called with a value greater than 0");
-  if (this.currentTemperature() + delta > this.maxTemp) throw new Error(`Temperature cannot go above ${this.maxTemp}`);
-  this.temp += delta;
-};
 
-Thermostat.prototype.down = function(delta = 1 ) {
-  if (delta <= 0) throw new Error("Down must be called with a value greater than 0");
-  if (this.currentTemperature() - delta < this.MIN_TEMP) throw new Error("Temperature cannot go below 10");
-  this.temp -= delta;
-};
 
-Thermostat.prototype.psmToggle = function() {
-  this.psmIsOn = !(this.psmIsOn);
-  this.maxTemp = this.psmTurnedOn() == true ? 25 : 32;
+
+
+
+
+
+function myFunction() {
+	alert("onclick() called");
 }
-
-Thermostat.prototype.reset = function() {
-  this.temp = 20;
-}
-
-Thermostat.prototype.currentEnergyUsage = function() {
-  return this.currentTemperature() < 18 ? 'low-usage' : this.currentTemperature() <= 25 ? 'medium-usage' : 'high-usage';
-}
-
-Thermostat.prototype.psmTurnedOn = function() {
-  return this.psmIsOn
-}
-
-Thermostat.prototype.currentTemperature = function() {
-  return this.temp
-}
-
-thermostat = new Thermostat()
